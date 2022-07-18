171637eb7ddd70626e661a410e75a7e5d26346d4
waynechen <n0918679182@gmail.com>
2022-07-18 23:48:38 +0800

    Update 2022-07-18T15:48:30.436Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-let burgerMenu = document.querySelector('.burger-menu');
-let menu = document.querySelector('.menu');
-
-burgerMenu.addEventListener('click',()=>{
-  menu.classList.toggle('action');
-});
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var burgerMenu = document.querySelector('.burger-menu');
+var menu = document.querySelector('.menu');
+burgerMenu.addEventListener('click', function () {
+  menu.classList.toggle('action');
+});
+//# sourceMappingURL=all.js.map
