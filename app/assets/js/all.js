ee92daef1b89bc183dffa06684e3af52844dc79f
waynechen <n0918679182@gmail.com>
2022-07-31 13:08:41 +0800

    新增product頁

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,3 +4,25 @@ let menu = document.querySelector('.menu');
 burgerMenu.addEventListener('click',()=>{
   menu.classList.toggle('action');
 });
+
+$(document).ready(function () {
+
+  $('.optical-bt').click(function(event){
+    $(this).addClass('active');
+    $('.sunglasses-bt').removeClass('active');
+    $('.optical').removeClass('d-none');
+    $('.sunglasses').addClass('d-none');
+  });
+
+  $('.sunglasses-bt').click(function(event){
+    $(this).addClass('active');
+    $('.optical-bt').removeClass('active');
+    $('.optical').addClass('d-none');
+    $('.sunglasses').removeClass('d-none');
+  })
+
+
+
+
+
+});
